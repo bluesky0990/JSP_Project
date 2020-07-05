613a70c4b00483f8c6b6d718330bed9e35eeab57
HG <bluesky0990@naver.com>
2020-07-06 01:26:18 +0900

    SQL arrange, Add station data

---
+++ WebContent/example/SQL/DATA_STATION.sql
@@ -0,0 +1,79 @@
+insert into station values(0, '미도스지', '에사카');
+
+insert into station values(1, '미도스지', '히가시미쿠니');
+
+insert into station values(2, '미도스지', '신오사카');
+
+insert into station values(3, '미도스지', '니시나카지마미나미가타');
+
+insert into station values(4, '미도스지', '나카츠');
+
+insert into station values(5, '미도스지', '우메다');
+
+insert into station values(6, '미도스지', '요도야바시');
+
+insert into station values(7, '미도스지', '혼마치');
+
+insert into station values(8, '요츠바시', '혼마치');
+
+insert into station values(9, '미도스지', '신사이바시');
+
+insert into station values(10, '미도스지', '난바');
+
+insert into station values(11, '센니치마에', '난바');
+
+insert into station values(12, '요츠바시', '난바');
+
+insert into station values(13, '미도스지', '다이코쿠초');
+
+insert into station values(14, '요츠바시', '다이코쿠초');
+
+insert into station values(15, '미도스지', '도부츠엔마에');
+
+insert into station values(16, '사카이스지', '도부츠엔마에');
+
+insert into station values(17, '미도스지', '덴노지');
+
+insert into station values(18, '센니치마에', '노다한신');
+
+insert into station values(19, '센니치마에', '다마가와');
+
+insert into station values(20, '센니치마에', '아와자');
+
+insert into station values(21, '센니치마에', '니시나가호리');
+
+insert into station values(22, '센니치마에', '사쿠라가와');
+
+insert into station values(23, '센니치마에', '닛폰바시');
+
+insert into station values(24, '사카이스지', '닛폰바시');
+
+insert into station values(25, '센니치마에', '오사카우에혼마치');
+
+insert into station values(26, '센니치마에', '츠루하시');
+
+insert into station values(27, '센니치마에', '이마자토');
+
+insert into station values(28, '요츠바시', '니시우메다');
+
+insert into station values(29, '요츠바시', '히고바시');
+
+insert into station values(30, '요츠바시', '요츠바시');
+
+insert into station values(31, '요츠바시', '스미노에코엔');
+
+insert into station values(32, '사카이스지', '덴진바시스지로쿠초메');
+
+insert into station values(33, '사카이스지', '오기마치');
+
+insert into station values(34, '사카이스지', '미나미모리마치');
+
+insert into station values(35, '사카이스지', '기타하마');
+
+insert into station values(36, '사카이스지', '사카이스지혼마치');
+
+insert into station values(37, '사카이스지', '나가호리바시');
+
+insert into station values(38, '사카이스지', '에비스초');
+
+insert into station values(39, '사카이스지', '덴가차야');
